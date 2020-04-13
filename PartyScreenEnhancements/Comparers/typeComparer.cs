4ea64fff9857296c87c9d496344ca293ed908576
Valentijn Hol <valentijnhol@hotmail.nl>
2020-04-13 17:09:23 +0200

    Remove typeComparer

--- PartyScreenEnhancements/Comparers/typeComparer.cs
+++ PartyScreenEnhancements/Comparers/typeComparer.cs
@@ -1,42 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Xml.Serialization;
 using TaleWorlds.CampaignSystem;
-using TaleWorlds.Core;
 
 namespace PartyScreenEnhancements.Comparers
 {
     public class TypeComparer : PartySort
     {
-
-        private readonly PartySort _equalSorter;
-        private bool _descending;
-
-        public TypeComparer(PartySort equalSorter, bool descending)
+        public TypeComparer(PartySort equalSorter, bool descending) : base(descending, equalSorter)
         {
-            this._equalSorter = equalSorter;
-            this._descending = descending;
+        }
+
+        internal TypeComparer()
+        {
+
         }
 
         protected override int localCompare(CharacterObject x, CharacterObject y)
         {
-            if (_descending ? x.CurrentFormationClass < y.CurrentFormationClass : x.CurrentFormationClass > y.CurrentFormationClass)
+            if (Descending
+                ? x.CurrentFormationClass < y.CurrentFormationClass
+                : x.CurrentFormationClass > y.CurrentFormationClass) return 1;
+
+            if (y.CurrentFormationClass == x.CurrentFormationClass)
             {
-                return 1;
+                if (EqualSorter != null)
+                    return EqualSorter.Compare(x, y);
+                return 0;
             }
-            else if (y.CurrentFormationClass == x.CurrentFormationClass)
-            {
-                if (_equalSorter != null)
-                    return _equalSorter.Compare(x, y);
-                else
-                    return 0;
-            }
-            else
-            {
-                return -1;
-            }
+
+            return -1;
         }
     }
 }
