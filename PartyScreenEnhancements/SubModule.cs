ecc4709e536ee1dc5276dac02449fb6cc2ec1c61
Valentijn Hol <valentijnhol@hotmail.nl>
2020-05-05 15:00:24 +0200

    Cleanup of sprite loading

--- PartyScreenEnhancements/SubModule.cs
+++ PartyScreenEnhancements/SubModule.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using HarmonyLib;
 using PartyScreenEnhancements.Saving;
 using TaleWorlds.Core;
@@ -28,7 +29,7 @@ namespace PartyScreenEnhancements
             {
                 FileLog.Log($"PSE Config Load Exception: {e}");
             }
-
+            
             if (PartyScreenConfig.ExtraSettings.ShowVisualAdditions)
             {
                 _extender = new UIExtender("PartyScreenEnhancements");
@@ -64,46 +65,19 @@ namespace PartyScreenEnhancements
 
         public void InitialiseSprites()
         {
-            ResourceDepot uiresourceDepot = UIResourceManager.UIResourceDepot;
-            TwoDimensionEngineResourceContext resourceContext = UIResourceManager.ResourceContext;
-            SpriteData spriteData = UIResourceManager.SpriteData;
-            SpriteData spriteData2 = new SpriteData("PSESpriteData");
-            spriteData2.Load(uiresourceDepot);
-            Texture item = new Texture(new EngineTexture(TaleWorlds.Engine.Texture.CreateTextureFromPath("../../Modules/PartyScreenEnhancements/GUI/SpriteSheets/pse_icons/", "pse_icons_1.png")));
-            spriteData.SpriteCategories.Add("pse_icons", spriteData2.SpriteCategories["pse_icons"]);
-            spriteData.SpritePartNames.Add("pse_sort_icon", spriteData2.SpritePartNames["pse_sort_icon"]);
-            spriteData.SpriteNames.Add("pse_sort_icon", new SpriteGeneric("pse_sort_icon", spriteData2.SpritePartNames["pse_sort_icon"]));
-            spriteData.SpritePartNames.Add("pse_empty_button", spriteData2.SpritePartNames["pse_empty_button"]);
-            spriteData.SpriteNames.Add("pse_empty_button", new SpriteGeneric("pse_empty_button", spriteData2.SpritePartNames["pse_empty_button"]));
-            SpriteCategory spriteCategory = spriteData.SpriteCategories["pse_icons"];
+            var oldSpriteData = UIResourceManager.SpriteData;
+            var newSpriteData = new SpriteData("PSESpriteData");
+            newSpriteData.Load(UIResourceManager.UIResourceDepot);
+            var item = new Texture(new EngineTexture(TaleWorlds.Engine.Texture.CreateTextureFromPath("../../Modules/PartyScreenEnhancements/GUI/SpriteSheets/pse_icons/", "pse_icons_1.png")));
+
+            newSpriteData.SpriteCategories.ToList().ForEach(kvp => oldSpriteData.SpriteCategories.Add(kvp.Key, kvp.Value));
+            newSpriteData.SpritePartNames.ToList().ForEach(kvp => oldSpriteData.SpritePartNames.Add(kvp.Key, kvp.Value));
+            newSpriteData.SpriteNames.ToList().ForEach(kvp => oldSpriteData.SpriteNames.Add(kvp.Key, kvp.Value));
+
+            var spriteCategory = oldSpriteData.SpriteCategories["pse_icons"];
             spriteCategory.SpriteSheets.Add(item);
-            spriteCategory.Load(resourceContext, uiresourceDepot);
-            UIResourceManager.BrushFactory.Initialize();
-        }
-
-        public void AddSprites(string spriteSheet, int sheetId = 1)
-        {
-
-            // InitializeSprites();
-            //
-            // AddSprites("pse_icons");
-
-            var test = UIResourceManager.SpriteData.SpriteCategories;
-
-            SpriteCategory spriteCategory = UIResourceManager.SpriteData.SpriteCategories[spriteSheet];
             spriteCategory.Load(UIResourceManager.ResourceContext, UIResourceManager.UIResourceDepot);
-            var texture = TaleWorlds.Engine.Texture.LoadTextureFromPath($"{spriteSheet}_{sheetId}.png",
-                $"{BasePath.Name}Modules/PartyScreenEnhancements/GUI/SpriteSheets/{spriteSheet}");
-            texture.PreloadTexture();
-            var texture2D = new TaleWorlds.TwoDimension.Texture(new EngineTexture(texture));
-            UIResourceManager.SpriteData.SpriteCategories[spriteSheet].SpriteSheets[sheetId - 1] = texture2D;
-        }
-
-        private void InitializeSprites()
-        {
-            UIResourceManager.UIResourceDepot.AddLocation("Modules/PartyScreenEnhancements/GUI/");
-            UIResourceManager.UIResourceDepot.CollectResources();
-            UIResourceManager.SpriteData.Load(UIResourceManager.UIResourceDepot);
+            UIResourceManager.BrushFactory.Initialize();
         }
     }
 }
