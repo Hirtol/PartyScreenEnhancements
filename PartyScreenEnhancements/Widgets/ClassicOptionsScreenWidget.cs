16f624c27f1d1253e1f9a23604930dbd417af93f
Valentijn Hol <valentijnhol@hotmail.nl>
2021-01-13 13:32:39 +0100

    Fix setting screen crash for 1.5.7

---
+++ PartyScreenEnhancements/Widgets/ClassicOptionsScreenWidget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.GauntletUI;
+using TaleWorlds.MountAndBlade.GauntletUI.Widgets.Options;
+using TaleWorlds.TwoDimension;
+
+namespace PartyScreenEnhancements.Widgets
+{
+	public class ClassicOptionsScreenWidget : OptionsScreenWidget
+	{
+        public ClassicOptionsScreenWidget(UIContext context) : base(context)
+		{
+		}
+
+		protected override void OnUpdate(float dt)
+		{
+			// Override to ensure that the PerformanceTabToggle doesn't get used, since the OptionsScreenWidget is sadly no longer
+			// as generic as it used to be.
+		} 
+
+	}
+}
