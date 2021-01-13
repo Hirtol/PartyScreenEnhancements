74121e66c30ad4cbb747ed5e9542cced495eb968
Valentijn Hol <valentijnhol@hotmail.nl>
2021-01-13 14:21:09 +0100

    Fix the descriptions of settings not popping up in the new update

--- PartyScreenEnhancements/Widgets/ClassicOptionsScreenWidget.cs
+++ PartyScreenEnhancements/Widgets/ClassicOptionsScreenWidget.cs
@@ -9,16 +9,36 @@ using TaleWorlds.TwoDimension;
 
 namespace PartyScreenEnhancements.Widgets
 {
+    /// <summary>
+    /// This is necessary since the OptionsScreenWidget now includes the PerformanceTab within the widget code, which means it's no longer as
+    /// general as it should be for our purposes. This override simply ensures the references to PerformanceTab are not reached, so that
+    /// we don't get a null pointer exception.
+    /// </summary>
 	public class ClassicOptionsScreenWidget : OptionsScreenWidget
-	{
+    {
+        private bool _initialized;
+
         public ClassicOptionsScreenWidget(UIContext context) : base(context)
 		{
 		}
 
 		protected override void OnUpdate(float dt)
 		{
-			// Override to ensure that the PerformanceTabToggle doesn't get used, since the OptionsScreenWidget is sadly no longer
-			// as generic as it used to be.
+            if (!this._initialized)
+            {
+                using (IEnumerator<Widget> enumerator = base.AllChildren.GetEnumerator())
+                {
+                    while (enumerator.MoveNext())
+                    {
+                        OptionsItemWidget optionsItemWidget;
+                        if ((optionsItemWidget = (enumerator.Current as OptionsItemWidget)) != null)
+                        {
+                            optionsItemWidget.SetCurrentScreenWidget(this);
+                        }
+                    }
+                }
+                this._initialized = true;
+            }
 		} 
 
 	}
